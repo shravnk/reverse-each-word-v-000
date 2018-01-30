415e5c59687e703b1665923d9b33c8fb85ecc365
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 15:03:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(phrase)
   words = phrase.split(" ")
   reversed = ""
   words.each do |word|
-    reversed << word.reverse
+    reversed << word.reverse + " "
   end
   puts reversed
 end
